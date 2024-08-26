f5c67f3fc6f3d3779b5c516746c551cd550f4f9b
MikhayloBil <152810994+MikhayloBil@users.noreply.github.com>
2024-08-26 18:15:06 +0300

    finish

--- server/src/utils/temperatureProcessor.ts
+++ server/src/utils/temperatureProcessor.ts
@@ -1,29 +1,26 @@
 import sharp from "sharp";
 import path from "path";
 
-const BINARY_DIMENSION_X = 3600; // Зменшені розміри для зображення
+const BINARY_DIMENSION_X = 3600;
 const BINARY_DIMENSION_Y = 1800;
 
 export async function processTemperatureFile(data: Buffer): Promise<Buffer> {
-  // Читання бінарного файлу
   const temperatures = [];
   for (let i = 0; i < data.length; i++) {
     temperatures.push(data.readInt8(i));
   }
 
-  // Завантаження порожньої карти світу
   const emptyMapPath = path.resolve(
     __dirname,
     "../../../temperature/public/empty-map.jpg"
   );
   const mapImage = await sharp(emptyMapPath)
-    .resize(BINARY_DIMENSION_X, BINARY_DIMENSION_Y) // Масштабування карти до розміру
+    .resize(BINARY_DIMENSION_X, BINARY_DIMENSION_Y)
     .raw()
     .toBuffer();
-  // Застосування температурних даних до зображення карти
+
   const coloredImage = applyTemperatureDataToImage(mapImage, temperatures);
 
-  // Використання sharp для збереження нового зображення
   const output = await sharp(coloredImage, {
     raw: {
       width: BINARY_DIMENSION_X,
@@ -44,23 +41,19 @@ function applyTemperatureDataToImage(
   const width = BINARY_DIMENSION_X;
   const height = BINARY_DIMENSION_Y;
 
-  // Ініціалізувати буфер для нових пікселів (RGB)
   const newImage = Buffer.from(mapImage);
 
-  // Обробка кожного пікселя
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
-      const idx = (y * width + x) * 3; // Індекс у буфері зображення
+      const idx = (y * width + x) * 3;
 
       const temp = temperatures[y * width + x];
 
-      // Перетворення температури на колір
       const color = temperatureToColor(temp);
 
-      // Застосувати колір до нового зображення
-      newImage[idx] = color.r; // Red
-      newImage[idx + 1] = color.g; // Green
-      newImage[idx + 2] = color.b; // Blue
+      newImage[idx] = color.r;
+      newImage[idx + 1] = color.g;
+      newImage[idx + 2] = color.b;
     }
   }
 
@@ -69,10 +62,10 @@ function applyTemperatureDataToImage(
 
 function temperatureToColor(temp: number): { r: number; g: number; b: number } {
   if (temp < 50) {
-    return { r: 0, g: 0, b: 255 }; // Синій для холодних температур
+    return { r: 0, g: 0, b: 255 };
   } else if (temp < 70) {
-    return { r: 0, g: 255, b: 0 }; // Зелений для помірних температур
+    return { r: 0, g: 255, b: 0 };
   } else {
-    return { r: 255, g: 0, b: 0 }; // Червоний для теплих температур
+    return { r: 255, g: 0, b: 0 };
   }
 }
